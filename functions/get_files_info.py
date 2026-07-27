df7c6316fd52990fc3f771e696bfef32d7ba84bf
GianniKilkenny <giannikilkenny22404@gmail.com>
2026-07-27 14:01:27 -0700

    Updated get_files_info() to list each item and it's meta data

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,9 +7,14 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([abs_path, target_path]) == abs_path
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        if os.path.isdir(directory) == False:
+        if os.path.isdir(target_path) == False:
             return f'Error: "{directory}" is not a directory'
-        
-        return f'Success: "{directory}" is within the working directory'
+        res = ""
+        for item in os.listdir(target_path):
+            item_path = os.path.join(target_path, item)
+            res += f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}\n"
+        return res
+            
+
     except Exception as e:
         return f'Error: {e}'
